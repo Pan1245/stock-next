e40e5358268415e0e9a06670f41896ad207184eb
Pan1245 <siwach.toprasert@gmail.com>
2023-02-01 15:52:06 +0700

    Add 'About' page.

--- pages/about.js
+++ pages/about.js
@@ -5,11 +5,11 @@ export default function AboutPage() {
   return (
     <>
       <Head>
-        <title>Siwach Page</title>
+        <title>About Page</title>
       </Head>
-      <h1>Siwach Page</h1>
-      <p>This is a sample page for Siwach</p>
-      <Link href="/about">About</Link>
+      <h1>About Page</h1>
+      <p>About Siwach.</p>
+      <Link href="/"></Link>
     </>
   );
 }
