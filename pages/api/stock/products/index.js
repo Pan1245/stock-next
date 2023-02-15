bbb14e2b2adf8071253c61dc2e9a0d1b286b71f0
Pan1245 <siwach.toprasert@gmail.com>
2023-02-15 16:48:30 +0700

    fix stock api

--- pages/api/stock/products/index.js
+++ pages/api/stock/products/index.js
@@ -15,8 +15,6 @@ export default async function handler(req, res) {
     res.setHander("Allow", ["GET", "POST"]);
     res.status(405).end("Method ${req.method} Not Allowed");
   }
-
-  res.status(200).json(docs);
 }
 
 const productSchema = new Schema({
